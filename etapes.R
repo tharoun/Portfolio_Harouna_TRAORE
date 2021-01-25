36d388c09b52380272702798a41d7f1c8bed523a
Harouna TRAORE <tharouna90@gmail.com>
2021-01-25 07:28:20 +0000

    description etapes

---
+++ etapes.R
@@ -0,0 +1,10 @@
+#Chagement du packages 
+library(blogdown)
+#installation de hugo
+blogdown::install_hugo()
+#creation du site en precisant le thème
+blogdown::new_site(theme = "hugo-toha/toha")
+#construction du site
+blogdown::build_site()
+#Ensuite on fait un git add -A dans la console
+#On cree un commit et on push sur github
